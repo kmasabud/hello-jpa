dfe4c28aec699109a5db2da01d0425ed0479e265
kmasabud <kmasabud@gmail.com>
2021-01-23 01:21:39 +0700

    Update ApplicationConstant.java
    change success to ok

--- src/main/java/com/earth/hellojpa/common/ApplicationConstant.java
+++ src/main/java/com/earth/hellojpa/common/ApplicationConstant.java
@@ -3,7 +3,7 @@ package com.earth.hellojpa.common;
 public class ApplicationConstant {
     public static final String VERIFY_TYPE_ALL = "VERIFY_TYPE_ALL";
     public static final String VERIFY_TYPE_SEARCH = "VERIFY_TYPE_SEARCH";
-    public static final String STATUS_OK = "SUCCESS";
+    public static final String STATUS_OK = "OK";
     public static final String STATUS_FAIL = "FAIL";
 
 }
